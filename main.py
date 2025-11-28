e99df8074ea27bc6b21e6ce40cc5f4f9b498a46c
Bluewizhat <blue.wizards.hat@gmail.com>
2025-11-28 17:05:46 -0600

    Add file path input functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from stats import words_in_text
 from stats import letter_count
 from stats import sort_counts
+import sys
 
 def get_book_text(book_path):
     with open(book_path) as f:
@@ -8,20 +9,24 @@ def get_book_text(book_path):
     return book_contents
 
 def main():
-    num_word = words_in_text(get_book_text("books/frankenstein.txt"))
-    letter_dic = letter_count(get_book_text("books/frankenstein.txt"))
-    sorted_dic = sort_counts(letter_dic)
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_word} total words")
-    print("--------- Character Count -------")
-    for count in sorted_dic:
-        if count["char"].isalpha():
-            print(f"{count['char']}: {count['num']}")
-        else:
-            continue
-    print("============= END ===============")
-    return
+    if len(sys.argv) == 2:
+        num_word = words_in_text(get_book_text(sys.argv[1]))
+        letter_dic = letter_count(get_book_text(sys.argv[1]))
+        sorted_dic = sort_counts(letter_dic)
+        print("============ BOOKBOT ============")
+        print(f"Analyzing book found at {sys.argv[1]}...")
+        print("----------- Word Count ----------")
+        print(f"Found {num_word} total words")
+        print("--------- Character Count -------")
+        for count in sorted_dic:
+            if count["char"].isalpha():
+                print(f"{count['char']}: {count['num']}")
+            else:
+                continue
+        print("============= END ===============")
+        return
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
 
 main()
